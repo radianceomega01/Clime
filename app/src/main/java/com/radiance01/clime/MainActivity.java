ff34fba7459f317cd0152a5f147fdd4a823ec32f
radianceomega01 <radianceomegadev@gmail.com>
2018-10-03 21:21:37 +0530

    after the first intent

---
+++ app/src/main/java/com/radiance01/clime/MainActivity.java
@@ -0,0 +1,42 @@
+package com.radiance01.clime;
+
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    ConstraintLayout backlayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        backlayout = findViewById(R.id.frame_lay);
+        animationDrawable = (AnimationDrawable) backlayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(1500);
+        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.start();
+
+        new CountDownTimer(8000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                Intent intent = new Intent(getApplicationContext(),MainContent.class);
+                startActivity(intent);
+            }
+        }.start();
+    }
+}
---
+++ app/src/main/java/com/radiance01/clime/MainContent.java
@@ -0,0 +1,16 @@
+package com.radiance01.clime;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainContent extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        //Intent intent = getIntent();
+    }
+}
