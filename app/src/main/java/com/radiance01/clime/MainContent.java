6133e4fcccdfdfe50ef647001e3400bb216e7bd8
radianceomega01 <radianceomegadev@gmail.com>
2018-10-04 21:33:02 +0530

    after location fetched and GET request

--- app/src/main/java/com/radiance01/clime/MainContent.java
+++ app/src/main/java/com/radiance01/clime/MainContent.java
@@ -1,16 +1,122 @@
 package com.radiance01.clime;
-
-import android.content.Intent;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class MainContent extends AppCompatActivity{
+
+    //http://openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    Double lat = 22.7196;
+    Double lon = 75.8577;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-        //Intent intent = getIntent();
+
+         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+         locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                volleyRequest();
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+
+         //--------------------------getting explicit location permission from the user-------------------------------
+
+
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
+        }
+
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        {
+            if(Build.VERSION.SDK_INT>23)
+            {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                {
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},0);
+                }
+            }
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        }
+    }
+
+
+    //---------------------------------------GET request to API from volley-----------------------------------------
+
+
+    public void volleyRequest()
+    {
+        String url = "http://openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&APPID=24f7fedb3acf45a83b2a07401f360d4f";
+
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(MainContent.this, "Response Recieved successfully", Toast.LENGTH_SHORT).show();
+                Log.v("clime","res:"+response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("clime","Error"+error.getLocalizedMessage());
+            }
+        });
+        Volley.newRequestQueue(this).add(jsonRequest);
+
     }
 }
+
+
